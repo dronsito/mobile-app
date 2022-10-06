750dc4aecfc13e1a78ac464775a9bbd7051554af
Fernando Tarango <fertarvega99@gmail.com>
2022-10-05 19:38:43 -0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'dronsito',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
